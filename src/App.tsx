462cff3f9875bfe241b64e09078bb98f5dcafcf3
rabiulhossen <rhsimon69@gmail.com>
2026-09-14 22:14:57 +0600

    feat: add basic terminal command handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,18 @@
+import {useState, type KeyboardEvent} from "react";
+
 function App() {
+
+  const [input,setInput] = useState("");
+  const [output, setOutput] = useState<string[]>([]);
+
+  const handleCommand = (event: React.KeyboardEvent<HTMLInputElement>) =>{
+    if (event.key !== "Enter"){
+   return;
+    }
+    if(input === "ls") {
+      setOutput(["firstFile.txt", "gems.txt"])
+    }
+  };
   return (
     <main className="terminal">
       <h1>TREASURE.SYS</h1>
@@ -7,12 +21,23 @@ function App() {
         <p>Welcome, deckhand.</p>
         <p>System initialized.</p>
         <p>Type "help" for available commands.</p>
+
+        {output.map((line, index) => (
+  <p key={index}>{line}</p>
+))}
       </div>
 
       <div className="prompt">
         <span>you@ship:~$</span>
-        <span>_</span>
+
+        <input
+          value={input}
+          onChange={(event) => setInput(event.target.value)}
+          onKeyDown={handleCommand}
+          autoFocus
+        />
       </div>
+      <p>Current command: {input}</p>
     </main>
   );
 }
